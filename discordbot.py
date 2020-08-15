89e44f81d5a16e99b65873f6ea800c4fc5ad12cf
ba77phone <69729240+ba77phone@users.noreply.github.com>
2020-08-16 02:48:22 +0900

    ishigoo

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,60 @@
+import discord
 from discord.ext import commands
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NzQ0MTAxNDYzNDM2ODIwNTMw.XzeUfQ.CT8t6jkhj4Kb7N_Fqk277QRmPPY'
+CH_ID = 744161340725133352
+discord_voice_channel_id = ''#732218077583900776
 
+voice = None
+player = None
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 
-bot.run(token)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+    channel = client.get_channel(CH_ID)
+
+    member_id = 742193673621471262
+    member = channel.guild.get_member(member_id)
+
+    
+    #await channel.send("密を避けるため、"f"{member.mention}を使って空気を入れ替えてください")
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    # 「/ishigo」と発言したら「」が返る処理
+    if message.content == '/ishigo':
+        await message.channel.send('私は今、宙に浮いている状態で...')
+        
+    global voich
+    # 接続
+    if message.content.startswith('!isgo'):
+        voich = await discord.VoiceChannel.connect(message.author.voice.channel)
+    # 切断
+    if message.content.startswith('!isout'):
+        await voich.disconnect()
+
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
+
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} はい？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
+
+    
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
